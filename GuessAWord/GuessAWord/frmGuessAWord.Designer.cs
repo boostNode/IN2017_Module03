ebcf7f5823c14e8fba749a270b963effec66d803
boostNode <support@boostnode.com>
2017-02-05 17:22:05 -0600

    Added GuessAWord

---
+++ GuessAWord/GuessAWord/frmGuessAWord.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace GuessAWord
+{
+    partial class frmGuessAWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGuessAWord));
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.lblDivider02 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.lblInstructions01 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.lblDivider01 = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.BackColor = System.Drawing.Color.DarkRed;
+            this.lblFeedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFeedback.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblFeedback.Location = new System.Drawing.Point(12, 240);
+            this.lblFeedback.Margin = new System.Windows.Forms.Padding(3);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Padding = new System.Windows.Forms.Padding(2);
+            this.lblFeedback.Size = new System.Drawing.Size(260, 60);
+            this.lblFeedback.TabIndex = 17;
+            this.lblFeedback.Text = "feedback";
+            this.lblFeedback.Visible = false;
+            // 
+            // lblDivider02
+            // 
+            this.lblDivider02.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblDivider02.Location = new System.Drawing.Point(12, 230);
+            this.lblDivider02.Margin = new System.Windows.Forms.Padding(3);
+            this.lblDivider02.Name = "lblDivider02";
+            this.lblDivider02.Size = new System.Drawing.Size(260, 4);
+            this.lblDivider02.TabIndex = 16;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Enabled = false;
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(149, 198);
+            this.txtGuess.MaxLength = 1;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(26, 26);
+            this.txtGuess.TabIndex = 15;
+            this.txtGuess.Text = "0";
+            this.txtGuess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuess.Location = new System.Drawing.Point(12, 198);
+            this.lblGuess.Margin = new System.Windows.Forms.Padding(3);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.lblGuess.Size = new System.Drawing.Size(140, 26);
+            this.lblGuess.TabIndex = 14;
+            this.lblGuess.Text = "Enter a letter (a-z):";
+            this.lblGuess.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblInstructions01
+            // 
+            this.lblInstructions01.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions01.ForeColor = System.Drawing.Color.Green;
+            this.lblInstructions01.Location = new System.Drawing.Point(12, 12);
+            this.lblInstructions01.Margin = new System.Windows.Forms.Padding(3);
+            this.lblInstructions01.Name = "lblInstructions01";
+            this.lblInstructions01.Size = new System.Drawing.Size(260, 72);
+            this.lblInstructions01.TabIndex = 13;
+            this.lblInstructions01.Text = resources.GetString("lblInstructions01.Text");
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Enabled = false;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(197, 198);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 26);
+            this.btnSubmit.TabIndex = 18;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnInit
+            // 
+            this.btnInit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInit.Location = new System.Drawing.Point(12, 90);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(75, 26);
+            this.btnInit.TabIndex = 19;
+            this.btnInit.Text = "Start";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // lblDivider01
+            // 
+            this.lblDivider01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblDivider01.Location = new System.Drawing.Point(12, 188);
+            this.lblDivider01.Margin = new System.Windows.Forms.Padding(3);
+            this.lblDivider01.Name = "lblDivider01";
+            this.lblDivider01.Size = new System.Drawing.Size(260, 4);
+            this.lblDivider01.TabIndex = 20;
+            // 
+            // lblWord
+            // 
+            this.lblWord.BackColor = System.Drawing.Color.Green;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblWord.Location = new System.Drawing.Point(12, 122);
+            this.lblWord.Margin = new System.Windows.Forms.Padding(3);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Padding = new System.Windows.Forms.Padding(2, 4, 2, 2);
+            this.lblWord.Size = new System.Drawing.Size(260, 60);
+            this.lblWord.TabIndex = 21;
+            this.lblWord.Text = "****";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblWord.Visible = false;
+            // 
+            // frmGuessAWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 312);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblDivider01);
+            this.Controls.Add(this.btnInit);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.lblDivider02);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblGuess);
+            this.Controls.Add(this.lblInstructions01);
+            this.Name = "frmGuessAWord";
+            this.Text = "Guess a Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFeedback;
+        private System.Windows.Forms.Label lblDivider02;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.Label lblInstructions01;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.Label lblDivider01;
+        private System.Windows.Forms.Label lblWord;
+    }
+}
+
---
+++ GuessAWord/GuessAWord/frmGuessAWord.cs
@@ -0,0 +1,64 @@
+﻿/*
+ *  Author: Troy Davis
+ *  Project: GuessAWord - GUI (Chapter 6, Programming Exercise 10)
+ *  Class: IN 2017 (Advanced C#)
+ *  Date: Feb 5, 2017 
+ *  Revision: Original
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessAWord
+{
+    // begin frmGuessAWord : Form
+    public partial class frmGuessAWord : Form
+    {
+        // class scope variables
+        int intRandomIndex;
+        Random ranNumberGenerator = new Random();
+
+        String[] strArrayWords = new String[] { "dog",
+                                                "cat",
+                                                "guess",
+                                                "syntax",
+                                                "iteration",
+                                                "method",
+                                                "array",
+                                                "expression" };
+
+        public frmGuessAWord()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        // begin Init()
+        //      Initializes form
+        private void Init()
+        {
+            // disable init (start) button
+            btnInit.Enabled = false;
+            // display random word
+            intRandomIndex = ranNumberGenerator.Next(0, strArrayWords.Length);
+            lblWord.Text = strArrayWords[intRandomIndex];
+            lblWord.Visible = true;
+            // enable submissions
+            txtGuess.Enabled = true;
+            btnSubmit.Enabled = true;
+        }
+        // end Init()
+    }
+    // end frmGuessAWord : Form
+}
